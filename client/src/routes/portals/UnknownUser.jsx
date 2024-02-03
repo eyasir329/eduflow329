3e40cb13984b29ed759f264e7e59ed31bd7f1cb6
Eyasir Ahamed <eyasir329@gmail.com>
2024-02-03 08:46:10 +0600

    guest ui

--- client/src/routes/portals/UnknownUser.jsx
+++ client/src/routes/portals/UnknownUser.jsx
@@ -1,7 +1,71 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
+
 export default function UnknownUser() {
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
   return (
-    <div>Guest</div>
+    <>
+      <div className='user-heading'>Guest User</div>
+      <div className='guest-content'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <form>
+              <img src={currentUser.profilePicture} alt='profile' className='circle-img'></img>
+              <div className='guest-extra'>
+                <p>Contact Admin To Access Your Account or <br/>  Enter Id and update your account</p>
+              </div>
+              <div className='guest-extra top'>
+              <input
+                    type='text'
+                    id='id'
+                    placeholder=' Enter Your ID'
+                    className='name'
+                  />
+              </div>
+              <div className='card'>
+                <div className='top'>
+                  <input
+                    type='text'
+                    defaultValue={currentUser.userName}
+                    id='username'
+                    placeholder='username'
+                    className='name'
+                  />
+                </div>
+                <div className='top'>
+                  <input
+                    type='email'
+                    defaultValue={currentUser.email}
+                    id='email'
+                    placeholder='Email'
+                    className=''
+                  />
+                </div>
+                <div className='top'>
+                  <input
+                    type='password'
+                    id='password'
+                    placeholder='Password'
+                    className=''
+                  />
+
+                </div>
+
+              </div>
+
+              <button type='button' className='btn btn-warning guest-button'>update</button>
+            </form>
+            <div className='guest-below'>
+              <span className='info1 info'>Delete Account</span>
+              <span className='info2 info'>Sign out</span>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </>
   )
 }

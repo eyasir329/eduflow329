8b8ae4cc358fd256beeff8a8730f6408fcd8d9de
Eyasir Ahamed <eyasir329@gmail.com>
2024-02-21 04:59:05 +0600

    parent portal v1

--- client/src/routes/portals/parent/Parent.jsx
+++ client/src/routes/portals/parent/Parent.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import Profile from "../../../components/functionality/Profile";
+import PresentInfo from "./PresentInfo";
+import ResultInfo from "./ResultInfo";
 
-export default function Parent(){
-    return(
-        <Profile 
-            title = "Parent"
-        />
+export default function Parent() {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <Profile
+                            title="Parent Portal"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="teacher-view-ex">
+                            <div className="teacher-view">
+                                <h1>Attendance Info</h1>
+                                <PresentInfo />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="teacher-view-ex">
+                            <div className="teacher-view">
+                                <h1>Result Info</h1>
+                                <ResultInfo />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
     );
 }
--- client/src/routes/portals/student/Student.jsx
+++ client/src/routes/portals/student/Student.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import Profile from "../../../components/functionality/Profile";
+import PresentInfo from "./PresentInfo";
+import ResultInfo from "./ResultInfo";
 
-export default function Student(){
-    return(
-        <Profile 
-            title = "Student"
-        />
-        
+export default function Student() {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <Profile
+                            title="Student Portal"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="teacher-view-ex">
+                            <div className="teacher-view">
+                                <h1>Attendance Info</h1>
+                                <PresentInfo />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="teacher-view-ex">
+                            <div className="teacher-view">
+                                <h1>Result Info</h1>
+                                <ResultInfo />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
     );
 }
